92ab2f1410a65f5c9b73e1ad7e70075bf681c67a
varshapal <varsha25.pal@gmail.com>
2023-08-26 15:56:17 +0530

    first commit

---
+++ ExpenseItem.js
@@ -0,0 +1,15 @@
+
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>Expense Items</h2>
+            <div>
+                <p>Food Rs 10</p>
+                <p>Petrol Rs 100</p>
+                <p>Movies Rs 200</p>
+            </div>
+        </div>
+    )
+
+}
+export default ExpenseItem;
