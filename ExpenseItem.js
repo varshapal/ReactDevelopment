de32bffad64912d9e885ce012b2e11ecb8743f5f
varshapal <35573741+varshapal@users.noreply.github.com>
2023-09-07 13:41:03 +0530

    add Expense Details component

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,7 +1,12 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
     
+    //const month = props.date.toLocalString('en-US', { month: 'long' });
+    // const day = props.date.toLocalString('en-US', { day: 'long2-digit' });
+    // const year = props.date.getFullYear();
     // var expenseItems = props.data.map(expense => 
     // <li key={expense.id}>
     // title={expense.title}
@@ -22,14 +27,20 @@ function ExpenseItem(props) {
         <div>{props.data.map(expense => 
             <div className="expense-item">
         
-            <div>{expense.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{expense.title}</h2>
-                <h2>{expense.location}</h2>
-                <div className="expense-item__price">${expense.amount}</div>
+                {/* <div>
+                    <div>{expense.date.toLocaleString('en-US', {month: 'long'})}</div>
+                    <div>{expense.date.getFullYear()}</div>
+                    <div>{expense.date.getDay()}</div>
+                </div> */}
+                <ExpenseDate date={expense.date}></ExpenseDate>
+                <div className="expense-item__description">
+                    {/* <h2>{expense.title}</h2>
+                    <h2>{expense.location}</h2>
+                    <div className="expense-item__price">${expense.amount}</div> */}
+                    <ExpenseDetails amount={expense.amount} location={expense.location} title={expense.title}></ExpenseDetails>
             </div>
             
-        </div>
+            </div>
         )}
         
         </div>
