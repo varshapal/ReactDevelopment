a5fc9ba12eb5aa1285ad28e21a7b2a3cbde17e1b
varshapal <35573741+varshapal@users.noreply.github.com>
2023-09-19 18:53:06 +0530

    update expense amount using useState

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,52 +1,30 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
-import Card from '../UI/Card';
 import './ExpenseItem.css';
+import Card from '../UI/Card';
 
-const ExpenseItem = (props) =>  {
-    
-    //const month = props.date.toLocalString('en-US', { month: 'long' });
-    // const day = props.date.toLocalString('en-US', { day: 'long2-digit' });
-    // const year = props.date.getFullYear();
-    // var expenseItems = props.data.map(expense => 
-    // <li key={expense.id}>
-    // title={expense.title}
-    // amount={expense.amount}
-    // date={expense.date}
-    // location={expense.location}
-    // </li>
-    // );
-    
-    //console.log(props.data);
+
+function ExpenseItem(props) {
+
+    const [title, setTitle] = useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+        setAmount('100');
+        console.log(amount);
+    }
     return (
-        // <div>{props.data.map(expense => 
-        //     <li>{expense.title} {expense.amount} {expense.location} {expense.date.toISOString()}</li>
-            
-
-        //     )}</div>
-        
-        <Card>{props.data.map(expense => 
-            <Card className="expense-item">
-        
-                {/* <div>
-                    <div>{expense.date.toLocaleString('en-US', {month: 'long'})}</div>
-                    <div>{expense.date.getFullYear()}</div>
-                    <div>{expense.date.getDay()}</div>
-                </div> */}
-                <ExpenseDate date={expense.date}></ExpenseDate>
-                <div className="expense-item__description">
-                    {/* <h2>{expense.title}</h2>
-                    <h2>{expense.location}</h2>
-                    <div className="expense-item__price">${expense.amount}</div> */}
-                    <ExpenseDetails amount={expense.amount} location={expense.location} title={expense.title}></ExpenseDetails>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
             </div>
-            
-            </Card>
-        )}
-        
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
-        
-    )
-
+    );
 }
+
 export default ExpenseItem;
