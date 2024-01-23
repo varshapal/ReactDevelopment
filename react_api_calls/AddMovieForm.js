034b2159147e3b4b8bb12017f2f7b48c233a9b41
varshapal <35573741+varshapal@users.noreply.github.com>
2024-01-23 16:55:10 +0530

    storing the new movie

--- react_api_calls/AddMovieForm.js
+++ react_api_calls/AddMovieForm.js
@@ -1,65 +1,105 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 import classes from "./AddMovieForm.module.css";
 
-const AddMovieForm = () => {
-  const [formData, setFormData] = useState({
-    title: "",
-    opening: "",
-    releasing: "",
-  });
-  const [dataList, setDataList] = useState([]);
+const AddMovieForm = (props) => {
+  const titleRef = useRef('');
+  const openingTextRef = useRef('');
+  const releaseDateRef = useRef('');
 
-  const inputChangeHandler = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const newMovieObj = {...formData };
-    setDataList((prevData) => [...prevData, newMovieObj ]);
-    setFormData({
-        title: '',
-        opening: '',
-        releasing: '',
-    })
-    console.log('new Movie', newMovieObj);
-  };
+    // add validation
+
+    const movie = {
+      title: titleRef.current.value,
+      openingText: openingTextRef.current.value,
+      releaseDate: releaseDateRef.current.value,
+    };
+
+    props.onAddMovie(movie);
+
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" ref={titleRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="opening-text">Opening Text</label>
+        <textarea rows='5' id="opening-text" ref={openingTextRef}></textarea>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="date">Release Date</label>
+        <input type="text" id="date" ref={releaseDateRef} />
+      </div>
+      <button>Add Movie</button>
+    </form>
+  )
+
+  }
+    
+// const AddMovieForm = () => {
+//   const [formData, setFormData] = useState({
+//     title: "",
+//     opening: "",
+//     releasing: "",
+//   });
+//   const [dataList, setDataList] = useState([]);
+
+//   const inputChangeHandler = (event) => {
+//     event.preventDefault();
+//     setFormData({ ...formData, [event.target.name]: event.target.value });
+//   };
+
+//   const submitHandler = (e) => {
+//     e.preventDefault();
+//     const newMovieObj = {...formData };
+//     setDataList((prevData) => [...prevData, newMovieObj ]);
+//     setFormData({
+//         title: '',
+//         opening: '',
+//         releasing: '',
+//     })
+//     console.log('new Movie', newMovieObj);
+//   };
 
   
-  return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <label>
-        Title
-        <input
-          type="text"
-          name="title"
-          value={formData.title}
-          onChange={inputChangeHandler}
-        />
-      </label>
-      <label>
-        Opening text
-        <input
-          type="opening"
-          name="opening"
-          value={formData.opening}
-          onChange={inputChangeHandler}
-        />
-      </label>
-      <label>
-        Release Date
-        <input
-          type="text"
-          name="release"
-          value={formData.releasing}
-          onChange={inputChangeHandler}
-        />
-      </label>
-      <button type="submit">Add Movie</button>
-    </form>
-  );
-};
+//   return (
+//     <form className={classes.form} onSubmit={submitHandler}>
+//       <label>
+//         Title
+//         <input
+//           type="text"
+//           name="title"
+//           value={formData.title}
+//           onChange={inputChangeHandler}
+//         />
+//       </label>
+//       <label>
+//         Opening text
+//         <input
+//           type="opening"
+//           name="opening"
+//           value={formData.opening}
+//           onChange={inputChangeHandler}
+//         />
+//       </label>
+//       <label>
+//         Release Date
+//         <input
+//           type="text"
+//           name="release"
+//           value={formData.releasing}
+//           onChange={inputChangeHandler}
+//         />
+//       </label>
+//       <button type="submit">Add Movie</button>
+//     </form>
+//   );
+// };
 
 export default AddMovieForm;
--- react_api_calls/App.js
+++ react_api_calls/App.js
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import AddMovieForm from "./components/AddMovieForm";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -10,63 +11,85 @@ function App() {
   //const [retryCount, setRetryCount] = useState(3);
   //const [cancelRetry, setCancelRetry] = useState(false);
 
-  
-
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      
-        const response = await fetch('https://swapi.dev/api/films');
-
-      if(!response.ok) {
-        throw new Error('something went wrong....Retrying');
+      const response = await fetch("https://react-http-9242d-default-rtdb.firebaseio.com/movies.json");
+      if (!response.ok) {
+        throw new Error("something went wrong....Retrying");
       }
-      
-      
+
       const data = await response.json();
-      
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
-      
+
+      const loadedMovies = [];
+
+      for ( const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+
+        })
+      }
+
+      // const transformedMovies = data.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
-        setError(error.message);
-        //setRetryCount(retryCount - 1);
-        
+      setError(error.message);
+      //setRetryCount(retryCount - 1);
     }
     setIsLoading(false);
     //setCancelRetry(false);
-    
   }, []);
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  async function addMovieHandler(movie) {
+    const response = await fetch("https://react-http-9242d-default-rtdb.firebaseio.com/movies.json", {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+
+  //timer for error message
   // useEffect(() => {
   //   if(retryCount > 0 && error ) {
   //     const retryTimer = setTimeout(() => {
   //       fetchMoviesHandler();
   //     }, 1000);
 
-  //     return () => 
+  //     return () =>
   //       clearTimeout(retryTimer);
   //     };
   //   }, [retryCount, error, cancelRetry]);
-  
-    // const cancelRetryHandler = () => {
-    //   setCancelRetry(true);
-    // }
+
+  // const cancelRetryHandler = () => {
+  //   setCancelRetry(true);
+  // }
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovieForm onAddMovie={addMovieHandler}/>
+      </section>
+
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
         {/* <button onClick={cancelRetryHandler}>Cancel</button> */}
@@ -74,11 +97,10 @@ function App() {
       <section>
         {/* {isLoading ? <span>Loading...</span> : <MoviesList movies={movies} />}
         {!isLoading && error && <p>{error}</p>} */}
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} /> }
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
         {!isLoading && error && <p>{error}</p>}
-        { isLoading && <p>Loading...</p>}
-        
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
