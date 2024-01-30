8194a9120862400791039abd7b1768a5990da73f
varshapal <35573741+varshapal@users.noreply.github.com>
2024-01-30 23:47:08 +0530

    verify Email

--- expense_tracker_app/Home.js
+++ expense_tracker_app/Home.js
@@ -1,17 +1,40 @@
 import ProfilePage from "./ProfilePage";
 import { useHistory} from 'react-router-dom';
+import AuthContext from "../store/auth-context";
+import { useContext } from "react";
 
 
 const Home = () => {
     const history = useHistory();
+    const authCtx = useContext(AuthContext);
+
     const updateProfile = () => {
-        console.log('hi');
         history.push('/profile_page');
     }
+        const verifyEmailHandler = async () => {
+            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBL0Dxkr3qq-HpRREjZfDFI5--szzAAycs', {
+                method: 'POST',
+                body: JSON.stringify({
+                    requestType: 'VERIFY_EMAIL',
+                    idToken: `${authCtx.token}`
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    //'X-Firebase-Locale': "hi"
+                }
+            })
+            const data = await response.json();
+            console.log(data);
+
+            
+        }
+        
+    
     return(
         <div>
         <h1>Welcome to Expense Tracker</h1>
         <p>Your profile is incomplete<button onClick={updateProfile}>Complete Now</button></p><hr />
+        <button onClick={verifyEmailHandler}>Verify Email Id</button>
 
         </div>
     )
