9a5b2a9586f8f1f8d383e2422da07492410d30a4
varshapal <35573741+varshapal@users.noreply.github.com>
2024-02-02 21:57:27 +0530

    redux counter

--- redux-demo/redux-demo.js
+++ redux-demo/redux-demo.js
@@ -1,1 +1,31 @@
+const redux = require('redux');
 
+const counterReducer = (state = {counter: 10 }, action) => {
+    switch(action.type) {
+        case "increment":
+            return state.counter + 1;
+
+        case "decrement": 
+            return state.counter - 1;
+
+        default:
+            return state;
+    }
+    
+};
+
+const store = redux.createStore(counterReducer);
+
+console.log(store.getState());
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+// for(let i=1; i<5 ;i++) {
+//     store.dispatch({type: 'increment'});
+// }
+ store.dispatch({type: 'decrement'});
