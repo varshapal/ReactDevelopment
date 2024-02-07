cf90b3a48bd4059f00d650c394c570dc8aea27c2
varshapal <35573741+varshapal@users.noreply.github.com>
2024-02-07 23:47:32 +0530

    Edit User Details

--- expense_tracker_app/ProfilePage.js
+++ expense_tracker_app/ProfilePage.js
@@ -1,64 +1,113 @@
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useRef, useState, useCallback } from "react";
 import AuthContext from "../store/auth-context";
-
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const ProfilePage = () => {
-    const nameInputRef = useRef();
-    const photoUrlRef = useRef();
-    const authCtx = useContext(AuthContext);
-    console.log(authCtx);
-    const updateHandler = (e) => {
-        e.preventDefault();
+  const [profileList, setProfileList] = useState([]);
+  const nameInputRef = useRef();
+  const photoUrlRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
+  const updateHandler = async (e) => {
+    e.preventDefault();
 
-        const enteredName = nameInputRef.current.value;
-        const enteredPhotoUrl = photoUrlRef.current.value;
+    const enteredName = nameInputRef.current.value;
+    const enteredPhotoUrl = photoUrlRef.current.value;
 
-        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBL0Dxkr3qq-HpRREjZfDFI5--szzAAycs', {
-            method: 'POST',
-            body: JSON.stringify({
-                idToken: authCtx.token,
-                displayName: enteredName,
-                photoUrl: enteredPhotoUrl,
-                returnSecureToken: true
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then((res) => {
-            if(res.ok)  {
-                return res.json();
-            } else {
-                return res.json().then((data) => {
-                    let errorMessage = 'authentication failed';
-                    if(data && data.error && data.error.message) {
-                        errorMessage=data.error.meaasge;
-                    }
-                    throw new Error(errorMessage);
-                })
-            }
-        }).then((data) => {
-            console.log(data);
-        }).catch((err) => {
-            alert(err.message);
-        })
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBL0Dxkr3qq-HpRREjZfDFI5--szzAAycs",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: `${authCtx.token}`,
+          displayName: enteredName,
+          photoUrl: enteredPhotoUrl,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log("profile", data);
+    alert("profile completed");
 
-    };
+    const response1 = await fetch(
+      "https://react-http-9242d-default-rtdb.firebaseio.com/users.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: `${authCtx.token}`,
+          displayName: enteredName,
+          photoUrl: enteredPhotoUrl,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data1 = await response1.json();
+    console.log("profile", data1);
+  };
 
-    return(
-        <form onSubmit={updateHandler}>
-            <button>Cancel</button>
-            <label>
-                Full Name:
-                <input type="text" ref={nameInputRef}/>
-            </label>
-            <label>
-                Profile Photo URL
-                <input type="text" ref={photoUrlRef}/>
-            </label>
-            <button>update</button>
-        </form>
-    )
+  const fetchData = async () => {
+    const response2 = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBL0Dxkr3qq-HpRREjZfDFI5--szzAAycs",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: `${authCtx.token}`,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data2 = await response2.json();
+    console.log("data2", data2);
+
+    nameInputRef.current.value = data2.users[0].displayName;
+    photoUrlRef.current.value = data2.users[0].photoUrl;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <section>
+      <h3>Contact Details</h3>
+      <form onSubmit={updateHandler}>
+        <button>Cancel</button>
+        <label>
+          Full Name:
+          <input type="text" ref={nameInputRef} />
+        </label>
+        <label>
+          Profile Photo URL
+          <input type="text" ref={photoUrlRef} />
+        </label>
+        <button type="submit">update</button>
+      </form>
+
+      
+
+      <p>
+        <strong>Profile List</strong>
+        {profileList.map((item) => (
+          <p key={item.id}>
+            {item.name}- {item.photoUrl}-{" "}
+            {/* <button onClick={() => deteletExpense(item.id)}>Delete</button>{" "}
+            <button onClick={() => editHandler(item.id)}>Edit</button> */}
+          </p>
+        ))}
+      </p>
+      {/* <ProfileData users={profileList}/> */}
+    </section>
+  );
 };
 
 export default ProfilePage;
