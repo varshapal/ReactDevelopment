7e825085d97be64d363717b59ebfa0ab30162705
varshapal <35573741+varshapal@users.noreply.github.com>
2024-02-08 18:11:24 +0530

    Adding Premium Features (download csv file)

--- expense-tracker/Expense.js
+++ expense-tracker/Expense.js
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import classes from "./Expense.module.css";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { expenseActions } from "../store/index";
+import { expenseActions } from "../store/expense-slice";
+import { themeActions } from "../store/theme-slice";
+import ExpenseList from "./ExpenseList";
 
 const Expense = () => {
   const dispatch = useDispatch();
   const expense = useSelector((state) => state.expense.expenses);
   const total = useSelector((state) => state.expense.total);
+  const show = useSelector((state) => state.expense.showData);
+  const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
   const [formData, setFormData] = useState({
     expense: "",
     description: "",
@@ -41,81 +46,106 @@ const Expense = () => {
 
     const data = await response.json();
 
+    setFormData({ expense: "", description: "", category: "Food" });
 
-    //get data
+    //get Data
 
     const getRequest = await fetch(
       "https://react-http-9242d-default-rtdb.firebaseio.com/expenses.json"
     );
     const getData = await getRequest.json();
     console.log(getData);
-    const loadedExpense = [];
 
-    // for (const key in getData) {
-    //   loadedExpense.push({
-    //     id: key,
-    //     expense: getData[key].expense,
-    //     description: getData[key].description,
-    //     category: getData[key].category,
-    //   });
-      //console.log("data",loadedExpense);
-      dispatch(expenseActions.addExpense(expenses));
-      dispatch(expenseActions.total(Number(formData.expense)));
-    }
-  
+    dispatch(expenseActions.addExpense(expenses));
+    dispatch(expenseActions.total(Number(formData.expense)));
+    dispatch(expenseActions.showData());
+  };
 
+  const themeHandler = () => {
+    dispatch(themeActions.toggleTheme());
+  }
+
+
+  //download CSV file
+  const downloadHandler = () => {
+    const csvData = convertToCSV(expense);
+    const blob = new Blob([csvData], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'data.csv';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  }
+
+  const convertToCSV = (expense) => {
+    console.log(expense);
+    const headers = Object.keys(expense[0]).join(',');
+    const rows = expense.map(obj => Object.values(obj).join(',')).join('\n');
+    return `${headers}\n${rows}`;
+  };
+    
   
 
   return (
-    <section>
-      <form onSubmit={submitHandler}>
-        <label>
-          Expense
-          <input
-            type="number"
-            name="expense"
-            value={formData.expense}
-            onChange={inputChangeHandler}
-          />
-        </label>
-        <label>
-          Expense Description
-          <input
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={inputChangeHandler}
-          />
-        </label>
-        <label>
-          Category
-          <select
-            name="category"
-            value={formData.category}
-            onChange={inputChangeHandler}
-          >
-            <option value="Food">Food</option>
-            <option value="Petrol">Petrol</option>
-            <option value="salary">Salary</option>
-          </select>
-        </label>
-        <label>
-          Total Expense: <span>{total}</span>
-        </label>
-        <button type="submit">Add Expense</button>
-        {total > 10000 && <button>Premium</button>}
-      </form>
-      
-      {/* <p>
-        <strong>Expense List</strong>
+    <section className={classes.expense}>
+      <div
+        className={isDarkTheme ? classes["dark-theme"] : classes["light-theme"]}
+      >
+        <form onSubmit={submitHandler}>
+          <label>
+            Expense
+            <input
+              type="number"
+              name="expense"
+              value={formData.expense}
+              onChange={inputChangeHandler}
+            />
+          </label>
+          <label>
+            Expense Description
+            <input
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={inputChangeHandler}
+            />
+          </label>
+          <label>
+            Category
+            <select
+              name="category"
+              value={formData.category}
+              onChange={inputChangeHandler}
+            >
+              <option value="Food">Food</option>
+              <option value="Petrol">Petrol</option>
+              <option value="salary">Salary</option>
+            </select>
+          </label>
+          <label>
+            Total Expense: <span>{total}</span>
+          </label>
+          <button type="submit">Add Expense</button>
+          {/* <button onClick={showHandler}>Show data</button> */}
+          
+        </form>
+        {total > 10000 && <button onClick={themeHandler}>Premium</button>}
+        <h3>Expense List</h3>
         {expense.map((item) => (
+          <div><p><strong>Expense Item - Expense description - Expense</strong></p>
           <p key={item.id}>
-            {item.expense}- {item.description}- {item.category}{" "}
-            <button onClick={() => deteletExpense(item.id)}>Delete</button>{" "}
-            <button onClick={() => editHandler(item.id)}>Edit</button>
+            {item.expense} - {item.description} - {item.category}
+            
           </p>
+          </div>
         ))}
-      </p> */}
+
+        {isDarkTheme && <button onClick={downloadHandler}>Download CSV file</button>}
+        
+      </div>
     </section>
   );
 };
--- expense-tracker/index.js
+++ expense-tracker/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store/index';
+import { configureStore} from "@reduxjs/toolkit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import authSlice from "./auth-slice";
+import expenseSlice from "./expense-slice";
+import themeSlice from "./theme-slice";
+
+
+
+
+
+
+const store = configureStore({
+    reducer: {
+      auth: authSlice.reducer,
+      expense: expenseSlice.reducer,
+      theme: themeSlice.reducer,
+    },
+  });
+
+
+export default store;
