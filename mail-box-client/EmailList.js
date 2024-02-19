cbd21a330ee26c774c0c9df7ce90297060a3a13d
varshapal <35573741+varshapal@users.noreply.github.com>
2024-02-19 19:36:21 +0530

    delete mail

--- mail-box-client/EmailList.js
+++ mail-box-client/EmailList.js
@@ -1,13 +1,50 @@
-import EmailBody from './EmailBody';
-import classes from './EmailList.module.css';
-import EmailListSetting from './EmailListSetting';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import DeleteIcon from '@mui/icons-material/Delete';
+import classes from './InboxPage.module.css';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { inboxActions } from '../store/inbox-slice';
+const EmailList = ({id, to, sub, msg, time}) => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const isBlueDot = useSelector((state)=> state.inbox.isBlueDot);
+    
 
-const EmailList = () => {
+    const openMail = (id) => {
+        dispatch(inboxActions.openSelectedMail({to, sub, msg, time}));
+        history.push('/emaildetail')
+        
+    }
+
+    const deleteMail = async (id) => {
+        console.log("del",id);
+        try{
+            const response = await fetch(`https://react-http-9242d-default-rtdb.firebaseio.com/email/${id}.json`, {
+            method: 'DELETE',
+        });
+        if(!response.ok) {
+            throw new Error ('Failed to delete email');
+        }
+        console.log("detele email successfully");
+        
+        } catch(error) {
+            console.log(error);
+        }
+
+        
+    }
+     
     return(
-        <div>
-            <EmailListSetting />
-            <EmailBody />
+        
+        <div className={classes.emailList}>
+        <div className={classes.list} onClick={openMail}>
+            <CheckBoxOutlineBlankIcon/>
+            {isBlueDot && <div className={classes.bluedot}></div>}
+            <p>{to} {sub} {msg} {time}</p>
         </div>
+        <DeleteIcon className={classes.deleteIcon} onClick={() => deleteMail(id)}/> 
+        </div>  
+        
         
     )
 };
