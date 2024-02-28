1351a5c92c9c198012639d12649a1cfc9ddc5b19
varshapal <35573741+varshapal@users.noreply.github.com>
2024-02-29 00:34:19 +0530

    Dynamic routing [DIY}

--- meetup-project/index.js
+++ meetup-project/index.js
@@ -1,1 +1,11 @@
+import { Fragment} from 'react';
 
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails () {
+    return(
+        <MeetupDetail image='https://plus.unsplash.com/premium_photo-1663050685796-3624fdc2b4df?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' title='A First Meetup' address="some street 5, some city" description="This is a first meetup" />
+    )
+};
+
+export default MeetupDetails;
