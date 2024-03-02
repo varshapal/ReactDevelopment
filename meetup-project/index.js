e5c8c57bd491d3916161a566b89bead73d5bc82d
varshapal <35573741+varshapal@users.noreply.github.com>
2024-03-02 23:46:57 +0530

    make website SEO friendly

--- meetup-project/index.js
+++ meetup-project/index.js
@@ -1,17 +1,25 @@
 import { MongoClient, ObjectId } from 'mongodb';
-
+import Head from 'next/head';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { Fragment } from 'react';
 
 
 function MeetupDetails (props) {
     
     return(
-        <MeetupDetail 
-            image={props.meetupData.image}
-            title={props.meetupData.title}
-            address={props.meetupData.address}
-            description={props.meetupData.description}
-        />
+        <Fragment>
+            <Head>
+                <title>{props.meetupData.title}</title>
+                <meta name='description' content={props.meetupData.description} />
+            </Head>
+            <MeetupDetail 
+                image={props.meetupData.image}
+                title={props.meetupData.title}
+                address={props.meetupData.address}
+                description={props.meetupData.description}
+            />
+        </Fragment>
+        
     )
 };
 
